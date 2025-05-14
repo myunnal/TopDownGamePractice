17f448257178202c2bf881629e400b7324cb52eb
Myunna <laptevaite@gmail.com>
2025-05-15 00:36:32 +0300

    First push

---
+++ Assets/MenuController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menuCanvas;
+    void Start()
+    {
+        menuCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+        }
+    }
+}
